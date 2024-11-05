0ddd6abb393095cda2752a8e3cf287a1c31c5797
alexandre-girard <alexandre.girard@maif.fr>
2024-11-05 09:24:58 +0100

    ✨ add orderBy and order

--- src/api/characters-api.js
+++ src/api/characters-api.js
@@ -4,8 +4,21 @@ import characters from '../data/characters.json'
  * List of characters
  * @returns {Array} characters
  */
-export const getCharacters = () => {
-    return characters;
+export const getCharacters = (orderBy = 'name', order = 'asc') => {
+    // Sort characters by name
+    let sortedCharacters = characters.sort((a, b) => {
+        if (orderBy === 'name') {
+            return a.name.localeCompare(b.name)
+        } else {
+            throw new Error(`Cannot order by ${orderBy}`)
+        }
+    });
+
+    // Reverse the order if it's descending
+    if (order === 'desc') {
+        sortedCharacters = sortedCharacters.reverse();
+    }
+    return sortedCharacters;
 }
 
 /**
