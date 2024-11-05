00e0372b071d9eb114fe53931007595a09635661
alexandre-girard <alexandre.girard@maif.fr>
2024-11-05 08:57:47 +0100

    add lcov

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,4 +7,6 @@ module.exports = {
     "src/**/*.{js,jsx}", // Collect coverage from all js or jsx files in src folder
     "!src/**/*.test.{js,jsx}", // Exclude test files from coverage
   ],
+  coverageReporters: ["text", "lcov"], // Add lcov as a coverage reporter
+  testResultsProcessor: "jest-sonar-reporter", // Add jest-sonar-reporter as a test results processor
 };
