ef054d8ff54062bf68da386b1d8689b85b5b68bd
alexandre-girard <alexandre.girard@maif.fr>
2024-11-16 09:04:31 +0100

    100 % coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,8 +4,18 @@ module.exports = {
     "^.+\\.jsx?$": "babel-jest",
   },
   collectCoverageFrom: [
-    "src/**/*.{js,jsx}", // Collect coverage from all js or jsx files in src folder
-    "!src/**/*.test.{js,jsx}", // Exclude test files from coverage
+    // Collect coverage from all js or jsx files in src folder
+    "src/**/*.{js,jsx}", 
+    // Exclude test files from coverage
+    "!src/**/*.test.{js,jsx}", 
+    // Exclude main.jsx from coverage
+    "!src/main.jsx",
+    // Exclude App.jsx from coverage
+    "!src/App.jsx", 
+    // Exclude Layout.jsx from coverage
+    "!src/Layout.jsx", 
+    // Exclude routes.jsx from coverage
+    "!src/routes.jsx",
   ],
   testResultsProcessor: 'jest-sonar-reporter',
 };
